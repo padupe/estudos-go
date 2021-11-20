8a6bbe7946d8defec293dac43e808512f375ebca
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-20 17:14:37 -0300

    feat(Fundamentos): Aula 001. Primeiro Codigo em GoLang. Dicas sobre uso de comentarios no Codigo.

---
+++ 02. fundamentos/001/primeiro_codigo.go
@@ -0,0 +1,25 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+E para usálos é necessário declara um ou vários imports
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa")
+}
+
+// Go possui um "lint" por padrão
+
+/*
+Sobre comentários em códigos:
+
+1. Priorize código legível e faça comentários que agregam valor!
+2. Evite comentários óbvios
+3. Durante o curso abuse dos Comentários, use-os como aliados para eu aprendizado
+
+*/
