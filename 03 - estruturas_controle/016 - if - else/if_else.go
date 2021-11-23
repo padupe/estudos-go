c973d939fbb2f77786a6b1851f822e7cdb81e0eb
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-23 19:18:04 -0300

    feat(03 - estruturas de controle/016 - if/else): Adicionado ao Projeto. Curso Go (Golang): Explorando a linguagem do Google.

---
+++ 03 - estruturas_controle/016 - if - else/if_else.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func imprimirResultado(nota float64) {
+	// O parâmetro não precisa ser informado entre parênteses
+	if nota >= 7 {
+		fmt.Println("Aprovado com nota", nota)
+	} else {
+		fmt.Println("Reprovado com nota", nota)
+	}
+}
+
+func main() {
+	imprimirResultado(7.3)
+	imprimirResultado(5.5)
+}
