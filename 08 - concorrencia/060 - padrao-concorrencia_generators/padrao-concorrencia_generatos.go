27e6ace03502b119e37e8e437eecad1c8a149d6e
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-29 22:08:23 -0300

    feat(08 - concorrencia/059 - channel-usando_range_close): Adicionado ao Projeto. Aula978 - Curso Go (Golang): Explorando a linguagem do Google.

---
+++ 08 - concorrencia/060 - padrao-concorrencia_generators/padrao-concorrencia_generatos.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Padrão Generator
+// Google I/O 2012 - Go Concurrency Patterns
+
+// <-chan : Canal somente Leitura
+
+func titulo(urls ...string) <-chan string {
+	canal := make(chan string)
+
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			regex, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			canal <- regex.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return canal
+}
+
+func main() {
+	titulo1 := titulo("https://www.madeiramadeira.com.br", "https://www.udemy.com")
+	titulo2 := titulo("https://www.cod3r.com.br", "https://www.amazon.com")
+
+	fmt.Println("Primeiros colocados:", <-titulo1, "|", <-titulo2)
+	fmt.Println("Segundos colocados:", <-titulo1, "|", <-titulo2)
+}
