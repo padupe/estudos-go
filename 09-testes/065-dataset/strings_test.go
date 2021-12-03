5b10a8b09d273139587683051735c00ea54f5b53
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-12-03 18:39:35 -0300

    feat(08 - concorrencia/065 - dataset): Adicionado ao Projeto. Aula 85 - Curso Go (Golang): Explorando a linguagem do Google.

---
+++ 09-testes/065-dataset/strings_test.go
@@ -0,0 +1,35 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) - índices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T) {
+	testes := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		// Estou procurando dentro da string o valor "Cod3r" e espero que sua posição seja a 0
+		{"Cod3r é show", "Cod3r", 0},
+		// Estou procurando uma string vazia e espero que sua posição seja 0
+		{"", "", 0},
+		// Estou procurando por 'opa', com O minúsculo, e espero que o resultado seja que NÃO existe. Ou seja, -1
+		{"Opa", "opa", -1},
+		// Estou procurando pela letra 'o', e no nome a letra ocupe a posição 2
+		{"Leonardo", "o", 2},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa: %v", teste)
+
+		atual := strings.Index(teste.texto, teste.parte)
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
