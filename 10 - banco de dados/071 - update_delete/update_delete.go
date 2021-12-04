292fbae956474ea7c073910832b344602973d3f5
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-12-04 16:55:10 -0300

    feat(10 - banco de dados/071 - update_delete): Adicionado ao Projeto. Aula 92 - Curso Go (Golang): Explorando a linguagem do Google.

--- 10 - banco de dados/071 - update_delete/update_delete.go
+++ 10 - banco de dados/071 - update_delete/update_delete.go
@@ -14,14 +14,14 @@ func main() {
 	}
 	defer db.Close()
 
+	// UPDATE
 	stmt, _ := db.Prepare("update usuarios set nome = ? where id = ?")
 
 	stmt.Exec("Maria Eduarda", 1)
 	stmt.Exec("João Paulo", 2)
 
+	// DELETE
 	stmt2, _ := db.Prepare("delete from usuarios where id = ?")
 
 	stmt2.Exec(3)
 }
-
-// git commit -m $'feat(10 - banco de dados/071 - update_delete): Adicionado ao Projeto.\nAula 92 - Curso Go (Golang): Explorando a linguagem do Google.'
